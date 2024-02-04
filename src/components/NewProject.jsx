4670d95698b3c951647b88d460e00035a31a7a9c
hidayatridwan <ridwan.nurulhidayat@gmail.com>
2024-02-04 11:18:34 +0700

    state add new project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,22 +1,15 @@
+import Input from "./Input";
+
 export default function NewProject() {
-    return <div>
-        <menu>
-            <li><button>Cancel</button></li>
-            <li><button>Save</button></li>
+    return <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+            <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
+            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
-            <p>
-                <label>Title</label>
-                <input type="text" />
-            </p>
-            <p>
-                <label>Description</label>
-                <textarea></textarea>
-            </p>
-            <p>
-                <label>Due Date</label>
-                <input type="date" />
-            </p>
+            <Input textarea={false} label="Title" type="text" />
+            <Input textarea={true} label="Description" />
+            <Input textarea={false} label="Due Date" type="date" />
         </div>
     </div>
 }
